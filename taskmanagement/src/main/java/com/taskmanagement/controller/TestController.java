4adca4909143de88bca367feda88a5ca7f60e7be
Lorencido <filip.lorencido@protonmail.com>
2025-06-23 15:29:25 +0200

    setup Spring and tested functionality

---
+++ taskmanagement/src/main/java/com/taskmanagement/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.taskmanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @GetMapping("/test")
+    public String printTest() {
+        return "Successfully tested Spring Boot";
+    }
+}
